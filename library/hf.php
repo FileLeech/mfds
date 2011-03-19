901d4f3f0bfaf8f082a1585a2abbc1f63343489f
Tim018 <Tim018@gmail.com>
2011-03-19 16:55:39 +0000

    working module for hotfile.com needs to be implemented in download.php

---
+++ library/hf.php
@@ -0,0 +1,49 @@
+<?php
+	//$dlLink ="http://hotfile.com/dl/110921239/506ac05/Thumbs.db.html";
+	
+	function hf_fetchCookieFile(){
+		global $cookieFolder;
+		
+		$args = "user=758948&pass=0nly4me";
+		$url = "http://hotfile.com/login.php";
+		
+		$cookiefile = "hf_cookie.txt";
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL,$url);
+		curl_setopt ($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $args);
+		curl_setopt($curl, CURLOPT_COOKIEJAR, $cookiefile);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$msg = curl_exec($curl);
+		curl_close ($curl);
+		
+//		debug($msg);	
+
+		return $cookiefile;
+	}
+	
+	function hf_getDlFilename($link){
+	$tmp = explode("/",$link);
+	return substr($tmp[6],0,strlen($tmp[6])-5);
+	}
+	
+	
+	function hf_load($dlFile,$dlLink,$dlFilename,$cookiefile
+	{
+			
+		$curl = curl_init();	
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_FILE,$dlFile);
+		curl_setopt($curl, CURLOPT_NOPROGRESS,false);
+		curl_setopt($curl, CURLOPT_PROGRESSFUNCTION,'curlCallback');
+		curl_setopt($curl, CURLOPT_BUFFERSIZE, 262144);
+		curl_setopt ($curl, CURLOPT_URL,$dlLink);
+		curl_setopt($curl, CURLOPT_COOKIEFILE, $cookiefile);
+		curl_exec($curl);
+		curl_close($curl);
+		die();
+	}
+	
+?>
